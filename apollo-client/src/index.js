01814c5bf5c614dfa3a5f7254db76ffe8c47cc6d
JirachaiHuad <jirachai_u@hotmail.com>
2020-07-26 14:32:39 +0700

    Add simple apollo client

---
+++ apollo-client/src/index.js
@@ -0,0 +1,55 @@
+import ApolloBoost, { gql } from 'apollo-boost';
+
+const client = new ApolloBoost({
+  uri: 'http://localhost:4000'
+});
+
+const getUsers = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`;
+
+client.query({ query: getUsers }).then((response) => {
+  let html = '';
+
+  response.data.users.forEach(user => {
+    html += `
+      <div>
+        <h3>${user.name}</h3>
+      </div>
+    `;
+  });
+
+  document.getElementById('users').innerHTML = html;
+});
+
+const getPosts = gql`
+  query {
+    posts {
+      title
+      author {
+        name
+      }
+    }
+  }
+`;
+
+client.query({ query: getPosts }).then((response) => {
+  let html = '';
+
+  response.data.posts.forEach(post => {
+    html += `
+      <div>
+        <h3>${post.title}</h3>
+        <i>by: ${post.author.name}</i>
+      </div>
+    `;
+  });
+
+  document.getElementById('posts').innerHTML = html;
+});
+
